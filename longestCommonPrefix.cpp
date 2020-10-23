3f5f2b91ec689f78df72c5394a39d5ebe9298834
Cyrus David Pastelero <61748886+Cyvid7-Darus10@users.noreply.github.com>
2020-10-23 14:02:27 +0800

    Create longestCommonPrefix.cpp

---
+++ longestCommonPrefix.cpp
@@ -0,0 +1,6 @@
+  string longestCommonPrefix(vector<string>& s) {
+  if (s.empty()) return "";
+  for (auto j = 0; ; ++j)
+    for (auto i = 0; i < s.size(); ++i)
+      if (s[i].size() < j || (i != 0 && s[i][j] != s[i - 1][j])) return s[i].substr(0, j);
+}
